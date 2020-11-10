48ab4608925df30fbdf8e76b880dd9f8ed287775
abdullahbinshakeel <73660196+abdullahbinshakeel@users.noreply.github.com>
2020-11-10 12:04:45 +0500

    Inverse

---
+++ ABS.vb
@@ -0,0 +1,25 @@
+﻿
+Module middleStr
+
+    Sub main()
+        'DECLERATION
+        Dim x As String = ""
+        Dim myChar As Char = ""
+        Dim C As Integer = 0
+        Dim L1 As Integer = 0
+
+        Console.Write("Enter The String: ")
+        x = Console.ReadLine
+
+        L1 = Len(x)
+
+        For C = L1 To 1 Step -1
+            myChar = Mid(x, C, 1)
+            Console.Write(myChar)
+        Next
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
