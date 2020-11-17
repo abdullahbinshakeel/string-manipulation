edfbb223813e1fbc24873b56a69f96e9c2d1fa3e
abdullahbinshakeel <73660196+abdullahbinshakeel@users.noreply.github.com>
2020-11-17 11:32:59 +0500

    Add files via upload

---
+++ Module1   fibonacci series..vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        'decleration
+        Dim l, x, y, sum As Integer
+        Console.Write("Enter Max Value: ")
+
+        'initialisation
+        l = Console.ReadLine
+        sum = 0
+        x = 0
+        y = 1
+        Console.Write(x & " , " & y)
+        While sum <= l
+            sum = x + y
+            Console.Write(" , " & Str(sum))
+            x = y
+            y = sum
+
+        End While
+        Console.ReadKey()
+
+    End Sub
+
+End Module
